b13645fca24a1ea5c1857668a6b0dcc6c6efa792
Lembert24 <albertlembert0708@gmail.com>
2026-07-16 16:57:31 -0400

    Agregue la clase servicio en la carpeta Entidades

---
+++ AuraDental.Data/Entities/Servicio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraDental.Data.Entities
+{
+    public class Servicio
+    {
+        public int ServicioId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public int DuracionMinutos { get; set; }
+        public decimal Precio { get; set; }
+
+        // Baja lógica, igual que Usuario y Provincia en el Sprint 1
+        public bool Activo { get; set; } = true;
+    }
+}
