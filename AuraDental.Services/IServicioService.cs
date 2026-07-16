be277b43bb5e0392035c0134074db08c59a167f6
Lembert24 <albertlembert0708@gmail.com>
2026-07-16 17:11:42 -0400

    Agregue una nueva clase llamada IServicioService en Dental.Service

---
+++ AuraDental.Services/IServicioService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuraDental.Data.Entities;
+
+namespace AuraDental.Services
+{
+    public interface IServicioService
+    {
+        List<Servicio> ObtenerTodos();
+        Servicio? ObtenerPorId(int id);
+        void Crear(Servicio servicio);
+        void Actualizar(Servicio servicio);
+        void CambiarEstado(int id, bool activo);
+        bool ExisteNombre(string nombre, int? idExcluir = null);
+    }
+}
