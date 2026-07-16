c5496d4dd141adceb23f695d751de3fa37901d26
Paoly M Feliz <Paolymfeliz15@hotmail.com>
2026-07-16 18:17:18 -0400

    Migracion y actualizacion de base de datos agregada

---
+++ AuraDental.Data/Migrations/20260716221640_AgregarBloqueAgenda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuraDental.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarBloqueAgenda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BloquesAgenda",
+                columns: table => new
+                {
+                    BloqueAgendaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HoraInicio = table.Column<TimeSpan>(type: "time", nullable: false),
+                    HoraFin = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Disponible = table.Column<bool>(type: "bit", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloquesAgenda", x => x.BloqueAgendaId);
+                    table.ForeignKey(
+                        name: "FK_BloquesAgenda_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "UsuarioId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloquesAgenda_UsuarioId",
+                table: "BloquesAgenda",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BloquesAgenda");
+        }
+    }
+}
