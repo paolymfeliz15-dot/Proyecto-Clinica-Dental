362d9c9f6f75e30751eeb2745562fdab14c6462f
AaronR04 <aaronrosal97@gmail.com>
2026-07-20 22:25:50 -0400

    feat: implementar Consultar Disponibilidad filtrando por duración del servicio - Sprint 3 (HU-02)

--- AuraDental.Services/IAgendaService.cs
+++ AuraDental.Services/IAgendaService.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using AuraDental.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using AuraDental.Data.Entities;
+
 
 namespace AuraDental.Services
 {
@@ -12,10 +14,27 @@ namespace AuraDental.Services
     {
         List<BloqueAgenda> ObtenerTodos();
         List<BloqueAgenda> ObtenerPorFecha(DateTime fecha);
+
+        List<BloqueAgenda> ObtenerDisponiblesPorServicio(int servicioId);
         BloqueAgenda? ObtenerPorId(int id);
         (bool exito, string mensaje) Crear(BloqueAgenda bloque);
         (bool exito, string mensaje) Actualizar(BloqueAgenda bloque);
         void Eliminar(int id);
         bool ExisteSolapamiento(DateTime fecha, TimeSpan horaInicio, TimeSpan horaFin, int? idExcluir = null);
     }
-}
+    public List<BloqueAgenda> ObtenerDisponiblesPorServicio(int servicioId)
+        {
+            var servicio = _context.Servicios.Find(servicioId);
+            if (servicio == null) return new List<BloqueAgenda>();
+
+            // Solo bloques disponibles, futuros, y con duración suficiente para el servicio elegido
+            return _context.BloquesAgenda
+                .Include(b => b.Usuario)
+                .Where(b => b.Disponible
+                         && b.Fecha >= DateTime.Today
+                         && (b.HoraFin - b.HoraInicio).TotalMinutes >= servicio.DuracionMinutos)
+                .OrderBy(b => b.Fecha)
+                .ThenBy(b => b.HoraInicio)
+                .ToList();
+        }
+    }
---
+++ AuraDental.Web/Controllers/HomeController1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AuraDental.Services;
+using AuraDental.Web.Filters;
+
+namespace AuraDental.Web.Controllers
+{
+    [SessionAuthorize(RolRequerido = "Paciente")]
+    public class DisponibilidadController : Controller
+    {
+        private readonly IAgendaService _agendaService;
+        private readonly IServicioService _servicioService;
+
+        public DisponibilidadController(IAgendaService agendaService, IServicioService servicioService)
+        {
+            _agendaService = agendaService;
+            _servicioService = servicioService;
+        }
+
+        // GET: /Disponibilidad
+        public IActionResult Index()
+        {
+            // Solo servicios activos pueden ser elegidos para consultar disponibilidad
+            var servicios = _servicioService.ObtenerTodos()
+                .Where(s => s.Activo)
+                .ToList();
+
+            return View(servicios);
+        }
+
+        // GET: /Disponibilidad/Consultar?servicioId=3
+        public IActionResult Consultar(int servicioId)
+        {
+            var servicio = _servicioService.ObtenerPorId(servicioId);
+            if (servicio == null) return NotFound();
+
+            var bloques = _agendaService.ObtenerDisponiblesPorServicio(servicioId);
+
+            ViewBag.Servicio = servicio;
+            return View(bloques);
+        }
+    }
+}
