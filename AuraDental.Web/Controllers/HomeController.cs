230c5cd1ebf55c11e93359e08632f7b00aeffe26
AaronR04 <aaronrosal97@gmail.com>
2026-07-25 17:24:53 -0400

    implementar página de inicio institucional con misión, visión, valores y vista previa de servicios - Sprint 4 (HU-18) - Aaron

--- AuraDental.Web/Controllers/HomeController.cs
+++ AuraDental.Web/Controllers/HomeController.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AuraDental.Services;
 
 namespace AuraDental.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IServicioService _servicioService;
+
+        public HomeController(IServicioService servicioService)
+        {
+            _servicioService = servicioService;
+        }
+
         public IActionResult Index()
+        {
+            // Mostramos hasta 3 servicios destacados en la vista previa del inicio;
+            // el catálogo completo con imágenes llega en HU-20 (Sprint 5)
+            var servicios = _servicioService.ObtenerTodos()
+                .Where(s => s.Activo)
+                .Take(3)
+                .ToList();
+
+            return View(servicios);
+        }
+
+        public IActionResult PoliticaPrivacidad()
         {
             return View();
         }
