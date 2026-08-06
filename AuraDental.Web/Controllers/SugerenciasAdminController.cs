679e5c7f28061e1c6dec8b19f1450b043919a1cd
Paoly M Feliz <Paolymfeliz15@hotmail.com>
2026-08-06 17:13:16 -0400

    feat: implementar campana de notificaciones para sugerencias no leídas usando View Component - Sprint 6 (HU-28) - Paoly
    Closes #29

---
+++ AuraDental.Web/Controllers/SugerenciasAdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AuraDental.Aplicacion;
+using AuraDental.Web.Filters;
+
+namespace AuraDental.Web.Controllers
+{
+    [SessionAuthorize(RolRequerido = "Administrador")]
+    public class SugerenciasAdminController : Controller
+    {
+        private readonly ISugerenciaService _sugerenciaService;
+
+        public SugerenciasAdminController(ISugerenciaService sugerenciaService)
+        {
+            _sugerenciaService = sugerenciaService;
+        }
+
+        public IActionResult Index()
+        {
+            var sugerencias = _sugerenciaService.ObtenerTodas();
+            return View(sugerencias);
+        }
+
+        [HttpPost]
+        public IActionResult MarcarLeida(int id)
+        {
+            _sugerenciaService.MarcarComoLeida(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AuraDental.Web/ViewComponents/CampanaSugerenciasViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AuraDental.Aplicacion;
+
+namespace AuraDental.Web.ViewComponents
+{
+    public class CampanaSugerenciasViewComponent : ViewComponent
+    {
+        private readonly ISugerenciaService _sugerenciaService;
+
+        public CampanaSugerenciasViewComponent(ISugerenciaService sugerenciaService)
+        {
+            _sugerenciaService = sugerenciaService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var noLeidas = _sugerenciaService.ContarNoLeidas();
+            return View(model: noLeidas);
+        }
+    }
+}
